42e8496bd73cc0e4b779e637cd49e93dd8eef2e3
yayayuuuuu <s111219031@stu.ntue.edu.tw>
2025-04-07 01:37:05 +0800

    h555

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer')
-    ]
-  }
+  plugins: [
+    require('tailwindcss'),   // 引入 tailwindcss 作為 PostCSS 插件
+    require('autoprefixer'),  // 引入 autoprefixer 來自動加前綴
+  ],
+};
